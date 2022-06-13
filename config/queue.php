7da10aeef6eadf818320ad5a45e864244d49ea0e
shakhawat <>
2022-06-14 02:25:04 +0600

    change quque timeout value

--- config/queue.php
+++ config/queue.php
@@ -40,6 +40,7 @@ return [
             'queue' => 'default',
             'retry_after' => 9000,
             'after_commit' => false,
+            'timeout' => 9100
         ],
 
         'beanstalkd' => [
